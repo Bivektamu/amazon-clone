60fafc8cef62d66c9123a1a55464ac18369789dd
Bivek Gurung <bivek.tamu@gmail.com>
2020-09-11 11:04:01 +1000

    Initialization of amazon clone frontend

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBj4u3bcMKEAJot0zaGiLWmC5NLZWoRiIA",
+    authDomain: "clone-ca2ec.firebaseapp.com",
+    databaseURL: "https://clone-ca2ec.firebaseio.com",
+    projectId: "clone-ca2ec",
+    storageBucket: "clone-ca2ec.appspot.com",
+    messagingSenderId: "357154841487",
+    appId: "1:357154841487:web:af04ee178c5b5c7b5621fa",
+    measurementId: "G-CXMC05TH8X"
+  };
