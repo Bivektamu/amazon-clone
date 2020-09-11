3fb9c58a006c81f7e6ac2bc927ccb2346c0444a4
Bivek Gurung <bivek.tamu@gmail.com>
2020-09-11 14:32:42 +1000

    Header and home page completed

---
+++ src/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Product.css';
+
+function Product({ title, img, price, rating }) {
+    return (
+        <div className='product'>
+            <div className="product__info">
+                <p>{title}</p>
+                <p className='product__price'>
+                    <small>$</small>
+                    <b>{price}</b>
+                </p>
+                <div className="product__rating">
+                    {Array(rating).fill().map((_, i) => (
+                        <p>⭐</p>
+                    ))}
+                </div>
+            </div>
+
+            <img src={img} alt=""></img>
+            <button>Add to Basket</button>
+        </div>
+    )
+}
+
+export default Product
