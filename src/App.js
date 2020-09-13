c5719adcabd9eeb5c55d12e6e1932a0fb5bca278
Bivek Gurung <bivek.tamu@gmail.com>
2020-09-13 22:31:33 +1000

    Login page and authentication feature completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+import Login from './Login';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
+
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 function App() {
+
+  const [{ }, dispatch] = useStateValue();
+
+
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      // console.log('user is >>>', authUser);
+
+      if (authUser) {
+        //  user logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      }
+      else {
+        // User logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+
+  }, [])
 
   // BEM convention
   return (
@@ -16,10 +45,23 @@ function App() {
 
     <div className="app">
       <Router>
-        <Header />
         <Switch>
-          <Route path='/checkout' component={Checkout} />
-          <Route path='/' component={Home} />
+          <Route path='/login'>
+            <Login />
+          </Route>
+
+          <Route path='/Checkout'>
+            <Header />
+            <Checkout />
+
+          </Route>
+
+          <Route path='/'>
+            <Header />
+            <Home />
+
+
+          </Route>
 
         </Switch>
       </Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,23 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBj4u3bcMKEAJot0zaGiLWmC5NLZWoRiIA",
-    authDomain: "clone-ca2ec.firebaseapp.com",
-    databaseURL: "https://clone-ca2ec.firebaseio.com",
-    projectId: "clone-ca2ec",
-    storageBucket: "clone-ca2ec.appspot.com",
-    messagingSenderId: "357154841487",
-    appId: "1:357154841487:web:af04ee178c5b5c7b5621fa",
-    measurementId: "G-CXMC05TH8X"
-  };
+  apiKey: "AIzaSyBj4u3bcMKEAJot0zaGiLWmC5NLZWoRiIA",
+  authDomain: "clone-ca2ec.firebaseapp.com",
+  databaseURL: "https://clone-ca2ec.firebaseio.com",
+  projectId: "clone-ca2ec",
+  storageBucket: "clone-ca2ec.appspot.com",
+  messagingSenderId: "357154841487",
+  appId: "1:357154841487:web:af04ee178c5b5c7b5621fa",
+  measurementId: "G-CXMC05TH8X"
+};
+
+// initialize app
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// intialize database
+const db = firebaseApp.firestore();
+
+// initialize authentication functions
+const auth = firebase.auth();
+
+export { db, auth };
